a7a1c339db3344dd47d6000f04e78218c11f80b8
Michal <mka@diveinai.com>
2025-11-18 16:54:19 +0100

    feat: Add AppGuidePage with instructions and info cards for concert attendees

---
+++ src/pages/AppGuidePage.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <lvgl.h>
+#include <ThemeColors.h>
+
+// App guide page with instructions for concert attendees
+class AppGuidePage {
+public:
+    static lv_obj_t* create();
+    static void cleanup();
+    
+private:
+    static lv_obj_t *screen;
+    static lv_obj_t *scroll_container;
+    
+    // Helper function to create info cards
+    static void create_info_card(lv_obj_t *parent, const char *icon, const char *title, const char *description, int y_offset);
+};
